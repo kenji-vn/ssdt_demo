c752f7a793ae02bd425fe5c5a12f126e0a4461ab
kenji_vn <thphuong299@gmail.com>
2016-11-18 10:30:09 +0700

    Init commit

---
+++ demo_database/demo_database/dbo/Stored Procedures/Customer_Insert.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE Customer_Insert
+	@FirstName nvarchar(40),
+	@LastName nvarchar(40),
+	@City nvarchar(40),
+	@Country nvarchar(40),
+	@Phone nvarchar(20)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Customer]
+           ([FirstName]
+           ,[LastName]
+           ,[City]
+           ,[Country]
+           ,[Phone])
+     VALUES
+           (@FirstName
+		   , @LastName
+		   , @City
+		   , @Country
+		   , @Phone)
+END
